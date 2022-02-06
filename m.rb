4173a7d0827d88ee97ec447747e1c75f206cef81
AwesomeNoob999 <51246943+AwesomeNoob999@users.noreply.github.com>
2022-02-05 20:22:10 -0600

    Update m.rb

--- m.rb
+++ m.rb
@@ -1,24 +1,24 @@
 class Entity
-def initialize
-x = y = z = 0
-x_vel = y_vel = 0
-x_accel = y_accel = 0
-image = []
-end
+	def initialize
+		x = y = z = 0
+		x_vel = y_vel = 0
+		x_accel = y_accel = 0
+		image = []
+	end
 end
 
 class Main
-def create_window(height, width)
-window = Array.new(height) {Array.new(width,'*') }
-$window = window
-@width = width
-@height = height
-end
-def update(origin, &block)
-@origin = origin
-row,column = @origin / @width, @origin % @width
-yield(@origin)
-end
+	def create_window(height, width)
+		window = Array.new(height) {Array.new(width,'*') }
+		$window = window
+		@width = width
+		@height = height
+	end
+	def update(origin, &block)
+		@origin = origin
+		row,column = @origin / @width, @origin % @width
+		yield(@origin)
+	end
 end
 
 e = Main.new
