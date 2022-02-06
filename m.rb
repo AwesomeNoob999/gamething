b3dfcb0a111b563c16ba43f75383939787cb70f1
AwesomeNoob999 <51246943+AwesomeNoob999@users.noreply.github.com>
2022-02-05 20:18:07 -0600

    Add files via upload

---
+++ m.rb
@@ -0,0 +1,27 @@
+class Entity
+def initialize
+x = y = z = 0
+x_vel = y_vel = 0
+x_accel = y_accel = 0
+image = []
+end
+end
+
+class Main
+def create_window(height, width)
+window = Array.new(height) {Array.new(width,'*') }
+$window = window
+@width = width
+@height = height
+end
+def update(origin, &block)
+@origin = origin
+row,column = @origin / @width, @origin % @width
+yield(@origin)
+end
+end
+
+e = Main.new
+e.create_window(11,11)
+p $window 
+e.update(73) { |e| p "#{e}"}
